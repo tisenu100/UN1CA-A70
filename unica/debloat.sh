5d261360a7c413daa94fde5e9f65a76d9c387911
tisenu100 <tisenu100@github.com>
2024-11-30 16:55:18 +0200

    Deprecate S23 Ultra firmware support

--- unica/debloat.sh
+++ unica/debloat.sh
@@ -32,7 +32,6 @@ etc/dpolicy
 SYSTEM_DEBLOAT+="
 system/bin/ssud
 system/etc/init/ssu_dm1qxxx.rc
-system/etc/init/ssu_dm3qxxx.rc
 system/etc/init/ssu.rc
 system/etc/permissions/privapp-permissions-com.samsung.ssu.xml
 system/etc/sysconfig/samsungsimunlock.xml
@@ -43,7 +42,6 @@ system/priv-app/SsuService
 
 # .odex files
 SYSTEM_DEBLOAT+="
-system/app/AirGlance/oat
 system/app/AllShareAware/oat
 system/app/AllshareMediaShare/oat
 system/app/ApexService/oat
@@ -80,7 +78,6 @@ system/app/HiyaService/oat
 system/app/HMT/oat
 system/app/HoneyBoard/oat
 system/app/KidsHome_Installer/oat
-system/app/LiveDrawing/oat
 system/app/LiveTranscribe/oat
 system/app/MAPSAgent/oat
 system/app/MdecService/oat
@@ -131,7 +128,6 @@ system/app/TetheringAutomation/oat
 system/app/UnifiedWFC/oat
 system/app/UniversalMDMClient/oat
 system/app/USBSettings/oat
-system/app/Uwbtest/oat
 system/app/VisionIntelligence3.7/oat
 system/app/VoiceAccess/oat
 system/app/VTCameraSetting/oat
@@ -163,8 +159,6 @@ system/framework/boot.vdex
 system/framework/boot-voip-common.vdex
 system/priv-app/Accessibility/oat
 system/priv-app/AdaptSound_U2/oat
-system/priv-app/AirCommand/oat
-system/priv-app/AirReadingGlass/oat
 system/priv-app/AppsEdgePanel_v3.2/oat
 system/priv-app/AppUpdateCenter/oat
 system/priv-app/AREmojiEditor/oat
