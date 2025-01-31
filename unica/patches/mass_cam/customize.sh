48c32b945aa3a29e7fcae914795b89cdaf4c0f25
tisenu100 <tisenu100@github.com>
2025-01-31 12:21:52 +0200

    unica: patches: mass_cam: Use Galaxy S24 FE's camera app instead

--- unica/patches/mass_cam/customize.sh
+++ unica/patches/mass_cam/customize.sh
@@ -27,18 +27,7 @@ if ! $SOURCE_HAS_MASS_CAMERA_APP; then
         REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/priv-app/SamsungCamera/oat"
         REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/priv-app/SamsungCamera/SamsungCamera.apk.prof"
         cp -a --preserve=all "$SRC_DIR/unica/patches/mass_cam/system/"* "$WORK_DIR/system/system"
-        if ! grep -q "FunModeSDK" "$WORK_DIR/configs/file_context-system"; then
-            {
-                echo "/system/app/FunModeSDK u:object_r:system_file:s0"
-                echo "/system/app/FunModeSDK/FunModeSDK\.apk u:object_r:system_file:s0"
-            } >> "$WORK_DIR/configs/file_context-system"
-        fi
-        if ! grep -q "FunModeSDK" "$WORK_DIR/configs/fs_config-system"; then
-            {
-                echo "system/app/FunModeSDK 0 0 755 capabilities=0x0"
-                echo "system/app/FunModeSDK/FunModeSDK.apk 0 0 644 capabilities=0x0"
-            } >> "$WORK_DIR/configs/fs_config-system"
-        fi
+
     else
         echo "TARGET_HAS_MASS_CAMERA_APP is not set. Ignoring"
     fi
