5eb00b8139d8177bd1b75acbbf5af8e758c078ae
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2026-06-20 16:27:43 +0300

    legacyui: patches: misc: Update miscs

--- target/a70q/patches/miscs/customize.sh
+++ target/a70q/patches/miscs/customize.sh
@@ -7,3 +7,4 @@ sed -i "/# Removed by /d" "$WORK_DIR/system/system/product/etc/build.prop" \
 echo "Remove unsupported permissions"
 DELETE_FROM_WORK_DIR "system" "system/etc/permissions/com.sec.feature.nsflp_level_601.xml"
 DELETE_FROM_WORK_DIR "system" "system/etc/permissions/com.sec.feature.sensorhub_level40.xml"
+DELETE_FROM_WORK_DIR "system" "system/etc/init/rscmgr_a36x.rc"
