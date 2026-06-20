81e939c73256516fa7e1069e0b92fe2f7876a35c
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2026-06-20 13:37:16 +0300

    legacyui: patches: brightness: Update for Android 16 QPR2

--- legacyui/patches/brightness/customize.sh
+++ legacyui/patches/brightness/customize.sh
@@ -1,4 +1,4 @@
 echo "Applying Adapative Brightness fix"
 HEX_PATCH "$WORK_DIR/system/system/lib64/libsensorservice.so" \
-    "f25a009420008052" "f25a009400008052"
+    "884b00942000805202000014e0031f2a" "884b00940000805202000014e0031f2a"
 echo "Adapative Brightness fix was applied successfully!"
